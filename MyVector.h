ae426da70dee30233af30d4f4d5813480c0de2b3
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-03-09 22:49:23 +0500

    ltssee

--- MyVector.h
+++ MyVector.h
@@ -1,1 +1,18 @@
+#ifndef MYVECTOR_H
+#define MYVECTOR_H
+// #include <iostream>
 
+class MyVector{
+private: int data[100]; int size;
+public:
+MyVector() : size(0) {}
+void push(int i){ if(size<100){ data[size++] = i;}}
+void pop{ if(size>0){ size--;}}
+int top() const { return (size>0 ? data[size-1] : -1);}
+bool isEmpty() const { return size == 0;}
+void printStack() const{ 
+    for(int i=(size-1); i>0; i++){ std::cout << data[i] << " ";} std::cout << std::endl;
+}
+};
+
+#endif

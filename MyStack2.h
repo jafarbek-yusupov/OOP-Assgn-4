ecaf96243560fa5715f8c2cf7d955c4d69afa510
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-03-09 23:16:15 +0500

    .

--- MyStack2.h
+++ MyStack2.h
@@ -1,12 +1,11 @@
 #ifndef MYSTACK2_H
 #define MYSTACK2_H
 #include "MyLinkedList.h"
-// #include <iostream>
 
 class MyStack2 {
 private: MyLinkedList* head;
 public: 
-MyStack() : head(nullptr) {}
+MyStack2() : head(nullptr) {}
 void push(int i){
     MyLinkedList* nn = new MyLinkedList(i);
     nn->next = head; head = nn;
@@ -14,7 +13,9 @@ void push(int i){
 void pop(){ if(head){ MyLinkedList* tmp = head; head = head->next; delete tmp;}}
 int top() const { return head ? head->value : -1;}
 bool isEmpty() const { return head==nullptr;}
-void printStack() const { for(MyLinkedList* cc=head; cc; cc->next){ std::cout << cc->value << " ";} std::cout << std::endl;}
+void printStack() const { 
+	for(MyLinkedList* cc = head; cc; cc = cc->next){ std::cout << cc->value << " ";} std::cout << std::endl;
+}
 };
 
 #endif

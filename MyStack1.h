f611940b421d727859a971c0f29192989b203f18
Jafarbek Yusupov <j.yusupov@newuu.uz>
2025-03-09 22:54:20 +0500

    lllllllllll

--- MyStack1.h
+++ MyStack1.h
@@ -1,1 +1,15 @@
+#ifndef MYSTACK1_H
+#define MYSTACK1_H
+#include "MyVector.h"
 
+class MyStack1{
+private: MyVector v;
+public:
+void push(int i){ v.push(i);}
+void pop(){ v.pop()}
+int top() const { v.top();}
+bool isEmpty() const { v.isEmpty();}
+void printStack() const{ v.printStack();}
+};
+
+#endif
